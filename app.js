cf6a01e289f9438f064de290c56b9d04c26f1e51
KESHAVBERG <kingkeshav7@gmail.com>
2022-01-15 10:43:48 +0530

    working on tabs

---
+++ app.js
@@ -0,0 +1,11 @@
+
+
+
+
+document.addEventListener("DOMContentLoaded", function(){
+    var elems = document.querySelectorAll('.tabs');
+    var instance = M.Tabs.init(elems, {
+        swipeable:false,
+    });
+
+})
